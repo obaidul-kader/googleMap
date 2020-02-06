b17a10e41c25f9a3a3b95e6af4c466f97e0e39ce
Guest User <Guest@labs-mac.local>
2020-02-06 16:13:31 +0600

    Initial Commit

---
+++ googleMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleMap
+//
+//  Created by Guest User on 2/6/20.
+//  Copyright © 2020 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
